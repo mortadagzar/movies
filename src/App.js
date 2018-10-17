18e3e437c412d8a6c948058082d641eaddafbe96
mortada gzar <mortadagzar@gmail.com>
2018-10-16 18:51:55 -0700

    h

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      rows:<h1>this is row</h1>
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -16,6 +22,8 @@ class App extends Component {
             </td>
           </tbody>
         </table>
+        <input  className="moviesearch" placeholder="Enter a movie name.." />
+        {this.state.rows}
       </div>
     );
   }
